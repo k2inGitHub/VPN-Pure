e5444b75e8143340b4e85a9610c9805c7a7ef933
songyang <aasyk2@126.com>
2016-02-22 13:55:06 +0800

    Initial Commit

---
+++ VPN-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VPN-iOS
+//
+//  Created by 宋扬 on 16/2/22.
+//  Copyright © 2016年 handloft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
